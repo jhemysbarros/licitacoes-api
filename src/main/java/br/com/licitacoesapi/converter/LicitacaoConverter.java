e02afdc9bfc6fd68934809dc8c4262390b3ab190
jhemysbarros <37225929+jhemysbarros@users.noreply.github.com>
2023-06-19 12:49:13 -0300

    :hammer: Adiciona interface Converter para mapear licitações e DTOs

---
+++ src/main/java/br/com/licitacoesapi/converter/LicitacaoConverter.java
@@ -0,0 +1,25 @@
+package br.com.licitacoesapi.converter;
+
+import br.com.licitacoesapi.model.Licitacao;
+import io.swagger.model.LicitacaoRequest;
+import io.swagger.model.LicitacaoResponse;
+import io.swagger.model.LicitacoesPaginada;
+import org.mapstruct.Mapper;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface LicitacaoConverter {
+	LicitacaoResponse toResponse(Licitacao licitacao);
+	List<LicitacaoResponse> toResponse(List<Licitacao> licitacoes);
+	Licitacao toDomain(Long id, LicitacaoRequest licitacaoRequest);
+
+	default LicitacoesPaginada toResponsePaginada(Page<Licitacao> licitacoes) {
+		LicitacoesPaginada licitacoesPaginada = new LicitacoesPaginada();
+		licitacoesPaginada.setLicitacoes(toResponse(licitacoes.toList()));
+		licitacoesPaginada.setPage(licitacoes.getNumber());
+		licitacoesPaginada.setSize(licitacoes.getSize());
+		return licitacoesPaginada;
+	}
+}
