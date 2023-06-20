c5a62a831a7ac306a67a11e7240a41fa92c0d74c
jhemysbarros <37225929+jhemysbarros@users.noreply.github.com>
2023-06-20 15:12:42 -0300

    :loud_sound: Melhora logs do Scheduler

--- src/main/java/br/com/licitacoesapi/scheduler/WebScrappingScheduler.java
+++ src/main/java/br/com/licitacoesapi/scheduler/WebScrappingScheduler.java
@@ -1,11 +1,15 @@
 package br.com.licitacoesapi.scheduler;
 
+import br.com.licitacoesapi.model.Licitacao;
 import br.com.licitacoesapi.service.WebScrappingService;
 import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.List;
 
 @Component
 @EnableScheduling
@@ -16,16 +20,16 @@ public class WebScrappingScheduler {
 
 	private static final String TIME_ZONE = "America/Sao_Paulo";
 	@Value("${cron.extrai-dados-da-web.job}")
-	private static final String CRON_EXTRAI_DADOS_DA_WEB_JOB = "0 5 * * * *";
+	private static final String CRON_EXTRAI_DADOS_DA_WEB_JOB = "0 2 * * * *";
 
 	public WebScrappingScheduler(WebScrappingService webScrappingService) {
 		this.webScrappingService = webScrappingService;
 	}
 
 	@Scheduled(cron = CRON_EXTRAI_DADOS_DA_WEB_JOB, zone = TIME_ZONE)
-	private void extraiDadosDaWeb() {
-		logger.info("Extraindo dados da Web...");
-		webScrappingService.extraiDadosDaWeb();
-		logger.infof("Dados da Web extraídos com succeso.");
+	private void extraiDadosDaWeb() throws IOException {
+		logger.info("Extraindo dados da Web para o banco de dados...");
+		List<Licitacao> licitacoesSalvas = webScrappingService.extraiDadosDaWeb();
+		logger.infof("%d licitações foram extraídas da Web para o banco de dados.", licitacoesSalvas.size());
 	}
 }
